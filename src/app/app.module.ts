6c9e2cff52a89cd2097e2db7c80ae4b90c8e58f4
Lisia <lisia.smienk@ziggo.nl>
2020-09-14 16:42:06 +0200

    opmaak globaal maken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,31 +8,31 @@ import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LeerlingTabelComponent } from './components/leerlingtabel/leerlingtabel.component';
 import { HttpClientModule} from '@angular/common/http';
-import { ViewLeerlingenPageComponent } from './view-leerlingen-page/view-leerlingen-page.component';
+import { ViewLeerlingenPageComponent } from './pages/view-leerlingen-page/view-leerlingen-page.component';
 import { KlastabelComponent } from './components/klastabel/klastabel.component';
-import { ViewKlassenPageComponent } from './view-klassen-page/view-klassen-page.component';
+import { ViewKlassenPageComponent } from './pages/view-klassen-page/view-klassen-page.component';
 import { VaktabelComponent } from './components/vaktabel/vaktabel.component';
-import { ViewVakkenPageComponent } from './view-vakken-page/view-vakken-page.component'
+import { ViewVakkenPageComponent } from './pages/view-vakken-page/view-vakken-page.component'
 import { DocenttabelComponent } from './components/docenttabel/docenttabel.component';
-import { ViewDocentenPageComponent } from './view-docenten-page/view-docenten-page.component';
-import { ViewDocentVakPageComponent } from './view-docent-vak-page/view-docent-vak-page.component';
+import { ViewDocentenPageComponent } from './pages/view-docenten-page/view-docenten-page.component';
+import { ViewDocentVakPageComponent } from './pages/view-docent-vak-page/view-docent-vak-page.component';
 import { KlasleerlingtabelComponent } from './components/klasleerlingtabel/klasleerlingtabel.component';
-import { ViewLeerlingenVanKlasPageComponent } from './view-leerlingen-van-klas-page/view-leerlingen-van-klas-page.component';
+import { ViewLeerlingenVanKlasPageComponent } from './pages/view-leerlingen-van-klas-page/view-leerlingen-van-klas-page.component';
 import { ToetsTabelComponent } from './components/toets-tabel/toets-tabel.component';
-import { ViewToetsenPageComponent } from './view-toetsen-page/view-toetsen-page.component';
-import { ViewToetsenVanVakPageComponent } from './view-toetsen-van-vak-page/view-toetsen-van-vak-page.component';
+import { ViewToetsenPageComponent } from './pages/view-toetsen-page/view-toetsen-page.component';
+import { ViewToetsenVanVakPageComponent } from './pages/view-toetsen-van-vak-page/view-toetsen-van-vak-page.component';
 import { ToetsVanVakTabelComponent } from './components/toets-van-vak-tabel/toets-van-vak-tabel.component';
 import { DocentCijferTabelComponent } from './components/docent-cijfer-tabel/docent-cijfer-tabel.component';
-import { ViewCijfersDocentPageComponent } from './view-cijfers-docent-page/view-cijfers-docent-page.component';
+import { ViewCijfersDocentPageComponent } from './pages/view-cijfers-docent-page/view-cijfers-docent-page.component';
 import { KlasVakkenTabelComponent } from './components/klas-vakken-tabel/klas-vakken-tabel.component';
-import { ViewKlasVakkenPageComponent } from './view-klas-vakken-page/view-klas-vakken-page.component';
+import { ViewKlasVakkenPageComponent } from './pages/view-klas-vakken-page/view-klas-vakken-page.component';
 import { KlasToetsenTabelComponent } from './components/klas-toetsen-tabel/klas-toetsen-tabel.component';
-import { ViewKlasToetsenPageComponent } from './view-klas-toetsen-page/view-klas-toetsen-page.component';
-import { ViewKlasPageComponent } from './view-klas-page/view-klas-page.component';
+import { ViewKlasToetsenPageComponent } from './pages/view-klas-toetsen-page/view-klas-toetsen-page.component';
+import { ViewKlasPageComponent } from './pages/view-klas-page/view-klas-page.component';
 import { DocentVakKlassenTabelComponent } from './components/docent-vak-klassen-tabel/docent-vak-klassen-tabel.component';
-import { ViewDocentVakKlassenPageComponent } from './view-docent-vak-klassen-page/view-docent-vak-klassen-page.component';
+import { ViewDocentVakKlassenPageComponent } from './pages/view-docent-vak-klassen-page/view-docent-vak-klassen-page.component';
 import { DocentKlassenTabelComponent } from './components/docent-klassen-tabel/docent-klassen-tabel.component';
-import { ViewDocentKlassenComponent } from './view-docent-klassen/view-docent-klassen.component';
+import { ViewDocentKlassenComponent } from './pages/view-docent-klassen/view-docent-klassen.component';
 import { DocentVakkenTabelComponent } from './components/docent-vakken-tabel/docent-vakken-tabel.component';
 import { FormsModule} from '@angular/forms';
 import { CijferService } from './service/cijfer.service';
@@ -43,7 +43,7 @@ import { ToetsService } from './service/toets.service';
 import { VakService } from './service/vak.service';
 import { DocentVakService } from './service/docent-vak.service';
 import { DocentKlasVakkenTabelComponent } from './components/docent-klas-vakken-tabel/docent-klas-vakken-tabel.component';
-import { ViewDocentKlasVakkenPageComponent } from './view-docent-klas-vakken-page/view-docent-klas-vakken-page.component';
+import { ViewDocentKlasVakkenPageComponent } from './pages/view-docent-klas-vakken-page/view-docent-klas-vakken-page.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/pages/view-klas-vakken-page/view-klas-vakken-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { KlasService } from '../../service/klas.service';
+import { KlasDto } from '../../models/KlasDto';
+
+@Component({
+  selector: 'app-view-klas-vakken-page',
+  templateUrl: './view-klas-vakken-page.component.html',
+  styleUrls: ['./view-klas-vakken-page.component.css']
+})
+export class ViewKlasVakkenPageComponent implements OnInit {
+
+  klas : KlasDto;
+
+  constructor(private ks: KlasService, private activatedrouter: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedrouter.paramMap.subscribe(params =>{
+      this.ks.findKlas(Number.parseInt(params.get("klasid"))).subscribe(klas =>{
+        this.klas = klas;
+      })
+    })
+  }
+
+}
